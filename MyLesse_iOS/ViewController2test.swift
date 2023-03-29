b74129403aec83936e873c233d1f4a07d809d1eb
UncleGeorge <george.cosmoswill@gmail.com>
2023-03-29 10:36:56 -0400

    testing
    testing the repo

---
+++ MyLesse_iOS/ViewController2test.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController2test.swift
+//  MyLesse_iOS
+//
+//  Created by Zhangzhi Wang on 2023-03-29.
+//
+
+import UIKit
+
+class ViewController2test: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
