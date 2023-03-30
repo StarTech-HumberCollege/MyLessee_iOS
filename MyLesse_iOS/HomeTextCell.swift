f2e4c6a697ad525a208b217ba8fe3ffa4f11f79c
UncleGeorge <george.cosmoswill@gmail.com>
2023-03-30 15:19:09 -0400

    finish home text tablecell layout
    finish home text tablecell layout

---
+++ MyLesse_iOS/HomeTextCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeTextCell.swift
+//  MyLesse_iOS
+//
+//  Created by Zhangzhi Wang on 2023-03-30.
+//
+
+import UIKit
+
+class HomeTextCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
