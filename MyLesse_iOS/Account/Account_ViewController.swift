ae7ca850daa35332d1840bb1871609ac89ae44b0
Alvaro Beltran <beltranalvaro08@gmail.com>
2023-03-31 18:34:27 -0400

    Added Account transition

---
+++ MyLesse_iOS/Account/Account_ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  Account_ViewController.swift
+//  MyLesse_iOS
+//
+//  Created by Alvaro Beltran on 2023-03-31.
+//
+
+import UIKit
+
+class Account_ViewController: UIViewController {
+
+    @IBOutlet weak var btn_logOut : UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         
+        btn_logOut.layer.cornerRadius = 20
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
